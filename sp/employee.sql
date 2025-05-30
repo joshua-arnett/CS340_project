659529bb080b3e57e0240c1804e05c55e596cd85
chengjuh <chengjuh@oregonstate.edu>
2025-05-30 00:01:06 -0700

    add sp for employee

---
+++ sp/employee.sql
@@ -0,0 +1,45 @@
+
+-- create
+DELIMITER //
+
+CREATE PROCEDURE AddEmployee (
+    IN deptID INT,
+    IN empName VARCHAR(25),
+    IN empPosition VARCHAR(100)
+)
+BEGIN
+    INSERT INTO Employees (departmentID, employeeName, employeePosition)
+    VALUES (deptID, empName, empPosition);
+END //
+
+DELIMITER ;
+
+
+-- delete--------------------------------------
+-- Don't delete employees with active orders!!!
+DELIMITER //
+
+CREATE PROCEDURE sp_DeleteEmployee(IN empID INT)
+BEGIN
+    DELETE FROM Employees WHERE employeeID = empID;
+END //
+
+DELIMITER ;
+
+
+-- Update --------------------------------------
+DELIMITER //
+
+CREATE PROCEDURE UpdateEmployee (
+    IN empID INT,
+    IN deptID INT,
+    IN empPosition VARCHAR(100)
+)
+BEGIN
+    UPDATE Employees
+    SET departmentID = deptID,
+        employeePosition = empPosition
+    WHERE employeeID = empID;
+END //
+
+DELIMITER ;
