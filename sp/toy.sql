2bc4b5f0134668bcf696327c89ba991c01bae327
LucyCheng111 <156858918+LucyCheng111@users.noreply.github.com>
2025-06-08 18:17:52 -0700

    Update toy.sql

--- sp/toy.sql
+++ sp/toy.sql
@@ -1,4 +1,8 @@
 
+-- citation: 
+-- chat GPT: https://chatgpt.com/share/68463511-3fc4-8013-a24a-a7b9e6f9935d
+-- Description: Give the database SQL and app.js code, and let it make CRUD sp functions
+
 -- create--------------------------------------
 DELIMITER //
 
